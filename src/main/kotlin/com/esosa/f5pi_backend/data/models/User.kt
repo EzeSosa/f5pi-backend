a37d2d24ba7226c047b784fcfa1662d10e426902
EzeSosa <ezequielsosa423@gmail.com>
2024-06-14 16:50:05 -0300

    fix: update mysql configuration

--- src/main/kotlin/com/esosa/f5pi_backend/data/models/User.kt
+++ src/main/kotlin/com/esosa/f5pi_backend/data/models/User.kt
@@ -5,11 +5,9 @@ import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
-import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
-@Table(name = "_user")
 data class User(
     val username: String,
     val password: String,
