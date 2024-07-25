6dff428a2bc0a3675db14649d47d36fe4a3d0dad
Matias <mmatidelga2@gmail.com>
2024-07-25 13:05:41 -0300

    fix: parameter index mismatch

--- src/main/kotlin/com/esosa/f5pi_backend/data/repositories/IGameRepository.kt
+++ src/main/kotlin/com/esosa/f5pi_backend/data/repositories/IGameRepository.kt
@@ -14,8 +14,8 @@ interface IGameRepository: JpaRepository<Game, UUID> {
             "g.user = ?1 " +
             "AND (?2 is null or g.date >= ?2) " +
             "AND (?3 is null or g.date <= ?3) " +
-            "AND (?5 is null or g.field = ?4) " +
-            "AND (?6 is null or g.season = ?5)"
+            "AND (?4 is null or g.field = ?4) " +
+            "AND (?5 is null or g.season = ?5)"
     )
     fun findByUser(
         user: User,
