ed948e68e86a991543bf99635118fea0a39dcf57
EzeSosa <ezequielsosa423@gmail.com>
2024-08-09 20:14:44 -0300

    ref: remove public id

--- src/main/kotlin/com/esosa/f5pi_backend/services/implementations/FileUploadService.kt
+++ src/main/kotlin/com/esosa/f5pi_backend/services/implementations/FileUploadService.kt
@@ -5,12 +5,10 @@ import com.esosa.f5pi_backend.services.interfaces.IFileUploadService
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
-import java.util.UUID
-
 @Service
 class FileUploadService(private val cloudinary: Cloudinary) : IFileUploadService {
     override fun uploadFile(multipartFile: MultipartFile): String =
         cloudinary.uploader()
-            .upload( multipartFile.bytes, mapOf( Pair("public_id", UUID.randomUUID()) ) )["url"]
+            .upload( multipartFile.bytes, emptyMap<String, Any>() )["url"]
             .toString()
 }
