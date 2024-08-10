3c68cb2a25e589d739d58cbe34117a721bbbcd66
EzeSosa <ezequielsosa423@gmail.com>
2024-08-10 20:08:29 -0300

    feat: image url in member response

--- src/main/kotlin/com/esosa/f5pi_backend/controllers/responses/MemberResponse.kt
+++ src/main/kotlin/com/esosa/f5pi_backend/controllers/responses/MemberResponse.kt
@@ -2,6 +2,7 @@ package com.esosa.f5pi_backend.controllers.responses
 
 data class MemberResponse(
     val playerName: String,
+    val imageURL: String,
     val goalsScored: Int,
     val ownGoals: Int
 )
