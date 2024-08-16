029a00ec211de3081e0b7540a42877a4bd1a869c
EzeSosa <ezequielsosa423@gmail.com>
2024-08-16 01:00:13 -0300

    ref: generate teams http request

--- src/main/kotlin/com/esosa/f5pi_backend/services/implementations/AIService.kt
+++ src/main/kotlin/com/esosa/f5pi_backend/services/implementations/AIService.kt
@@ -1,67 +1,61 @@
 package com.esosa.f5pi_backend.services.implementations
 
+import com.esosa.f5pi_backend.ai.AIClient
 import com.esosa.f5pi_backend.controllers.requests.GenerateTeamsRequest
 import com.esosa.f5pi_backend.controllers.responses.GenerateTeamsResponse
 import com.esosa.f5pi_backend.controllers.responses.PlayerResponse
 import com.esosa.f5pi_backend.controllers.responses.PlayerStatisticsResponse
 import com.esosa.f5pi_backend.data.models.Player
-import com.esosa.f5pi_backend.http.CustomHttpClient
+import com.esosa.f5pi_backend.ai.requests.QueryRequest
+import com.esosa.f5pi_backend.ai.responses.QueryResponse
+import com.esosa.f5pi_backend.ai.responses.QueryTeamsResponse
 import com.esosa.f5pi_backend.services.interfaces.IAIService
 import com.esosa.f5pi_backend.services.interfaces.IPlayerService
-import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
 
 @Service
 class AIService(
     private val playerService: IPlayerService,
-    private val httpClient: CustomHttpClient
+    private val aiClient: AIClient
 ) : IAIService {
 
     @Value("\${ai.message}")
     lateinit var BASE_MESSAGE: String
 
-    @Value("\${ai.url}")
-    lateinit var URL: String
-
     override fun generateTeams(generateTeamsRequest: GenerateTeamsRequest): GenerateTeamsResponse {
         val playerByName = mutableMapOf<String, Player>()
         val requestMessage = buildRequestMessage(generateTeamsRequest, playerByName)
-        val jsonResponse = httpClient.doRequest(URL, QueryRequest(requestMessage), HttpMethod.POST)
-        return jsonResponse.buildGenerateTeamsResponse(playerByName)
+        val response = aiClient.generateTeams(requestMessage)
+        return response.buildGenerateTeamsResponse(playerByName)
     }
 
-    private fun buildRequestMessage(generateTeamsRequest: GenerateTeamsRequest, playerByName: MutableMap<String, Player>): String =
-        buildString {
-            append(BASE_MESSAGE.trimIndent())
-            generateTeamsRequest.playersId.forEach { playerId ->
-                val player = playerService.findPlayerByIdOrThrowException(playerId)
-                val playerStatistics = playerService.getPlayerStatistics(playerId)
-                playerByName[player.name] = player
-                append(playerStatistics.listStatistics(player.name))
+    private fun buildRequestMessage(generateTeamsRequest: GenerateTeamsRequest, playerByName: MutableMap<String, Player>): QueryRequest =
+        QueryRequest(
+            buildString {
+                append(BASE_MESSAGE.trimIndent())
+                generateTeamsRequest.playersId.forEach { playerId ->
+                    val player = playerService.findPlayerByIdOrThrowException(playerId)
+                    val playerStatistics = playerService.getPlayerStatistics(playerId)
+                    playerByName[player.name] = player
+                    append(playerStatistics.listStatistics(player.name))
+                }
             }
-        }
+        )
 
     private fun PlayerStatisticsResponse.listStatistics(playerName: String): String = """
-        |Name: $playerName, Total matches: $games, 
-        |Wins: $wins, Draws: $draws, Losses: $losses,
-        |Goals scored: $goals, Own goals: $ownGoals
+        | Name: $playerName, Total matches: $games, 
+        | Wins: $wins, Draws: $draws, Losses: $losses,
+        | Goals scored: $goals, Own goals: $ownGoals
+        | -
     """.trimMargin()
 
-    private fun JsonNode.buildGenerateTeamsResponse(playerByName: Map<String, Player>): GenerateTeamsResponse {
-        val explanation = this["explanation"].asText()
-        val teams = this["teams"].buildTeamsList(playerByName)
-        return GenerateTeamsResponse(explanation, teams)
-    }
+    private fun QueryResponse.buildGenerateTeamsResponse(playerByName: Map<String, Player>) =
+        GenerateTeamsResponse(explanation, teams.buildTeams(playerByName))
 
-    private fun JsonNode.buildTeamsList(playerByName: Map<String, Player>): List<List<PlayerResponse>> =
-        this.fields().asSequence()
-            .map { it.value.toPlayerList(playerByName) }
-            .toList()
-
-    private fun JsonNode.toPlayerList(playerByName: Map<String, Player>): List<PlayerResponse> =
-        this.map { playerByName[it.asText()]!!.buildPlayerResponse() }
-
-    data class QueryRequest(val query: String)
+    private fun QueryTeamsResponse.buildTeams(playerByName: Map<String, Player>): List<List<PlayerResponse>> =
+        listOf(
+            team1.map { playerByName[it]!!.buildPlayerResponse() },
+            team2.map { playerByName[it]!!.buildPlayerResponse() }
+        )
 }
