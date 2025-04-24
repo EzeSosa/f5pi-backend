874d8963e431810f67e3573c8f44a2b9d066208f
MartinCrosetto <matincrosetto312@gmail.com>
2025-04-24 01:33:04 -0300

    Integration Tests for GET user fields

---
+++ src/test/kotlin/com/esosa/f5pi_backend/controllers/implementations/UserControllerTest.kt
@@ -0,0 +1,73 @@
+package com.esosa.f5pi_backend.controllers.implementations
+
+import com.esosa.f5pi_backend.controllers.base.BaseIntegrationTest
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.http.MediaType
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.util.*
+
+class UserControllerTest : BaseIntegrationTest()  {
+ @Test
+ fun `should return bad request when user does not exists`() {
+  val userId = UUID.randomUUID()
+    val pageNumber = 0
+    val pageSize = 2
+    val sortAttribute = "createdAt"
+    val sortOrder = "desc"
+
+    mockMvc.perform(
+     get("/api/v1/fields/{userId}/fields", userId)
+        .param("pageNumber", pageNumber.toString())
+        .param("pageSize", pageSize.toString())
+        .param("sortAttribute", sortAttribute)
+        .param("sortOrder", sortOrder)
+    )
+     .andExpect(status().isBadRequest)
+     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+     .andExpect(jsonPath("$.message").value("User with id $userId does not exist"))
+ }
+
+
+@Test
+@Sql("/scripts/fields/insert_fields.sql")
+ fun `should return ok when user has fields`() {
+   val userId = UUID.fromString("58fae25b-ea38-4e7b-ab2d-9f555a67836b")
+   val pageNumber = 0
+   val pageSize = 2
+   val sortAttribute = "createdAt"
+   val sortOrder = "desc"
+
+   mockMvc.perform(
+    get("/api/v1/fields/{userId}/fields", userId)
+     .param("pageNumber", pageNumber.toString())
+     .param("pageSize", pageSize.toString())
+     .param("sortAttribute", sortAttribute)
+     .param("sortOrder", sortOrder)
+   ).andExpect(status().isOk)
+     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+    .andExpect(jsonPath("$.content.length()").value(1))
+ }
+
+ @Test
+ @Sql("/scripts/fields/insert_fields.sql")
+ fun `should return not found when user does not have fields`() {
+   val userId = UUID.fromString("68fae25b-ea38-4e7b-ab2d-9f555a67836e")
+   val pageNumber = 0
+   val pageSize = 2
+   val sortAttribute = "createdAt"
+   val sortOrder = "desc"
+
+   mockMvc.perform(
+    get("/api/v1/fields/{userId}/fields", userId)
+     .param("pageNumber", pageNumber.toString())
+     .param("pageSize", pageSize.toString())
+     .param("sortAttribute", sortAttribute)
+     .param("sortOrder", sortOrder)
+   ).andExpect(status().isNotFound)
+     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+     .andExpect(jsonPath("$.message").value("User with id $userId has no fields"))
+ }
+
+}
--- src/test/resources/scripts/fields/insert_fields.sql
+++ src/test/resources/scripts/fields/insert_fields.sql
@@ -1,5 +1,6 @@
 INSERT INTO app_user (id, username, password, email)
-VALUES ('58fae25b-ea38-4e7b-ab2d-9f555a67836b', 'esosa', '123456', 'esosa@gmail.com');
+VALUES ('58fae25b-ea38-4e7b-ab2d-9f555a67836b', 'esosa', '123456', 'esosa@gmail.com'),
+       ('68fae25b-ea38-4e7b-ab2d-9f555a67836e', 'mcrosetto', 'abcdef', 'mcrosetto@gmail.com');
 
 INSERT INTO field (id, name, user_id)
 VALUES ('58fae25b-ea38-4e7b-ab2d-9f555a67836c', 'Field Test', '58fae25b-ea38-4e7b-ab2d-9f555a67836b');
