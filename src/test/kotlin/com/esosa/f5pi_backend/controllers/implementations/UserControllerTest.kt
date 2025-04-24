bf08631f645e865fc239ecb2786e60a03a13d01f
MartinCrosetto <matincrosetto312@gmail.com>
2025-04-24 15:39:58 -0300

    Fix: unnecessary variable declarations were removed

--- src/test/kotlin/com/esosa/f5pi_backend/controllers/implementations/UserControllerTest.kt
+++ src/test/kotlin/com/esosa/f5pi_backend/controllers/implementations/UserControllerTest.kt
@@ -13,17 +13,12 @@ class UserControllerTest : BaseIntegrationTest()  {
  @Test
  fun `should return bad request when user does not exists`() {
   val userId = UUID.randomUUID()
-    val pageNumber = 0
-    val pageSize = 2
-    val sortAttribute = "createdAt"
-    val sortOrder = "desc"
-
     mockMvc.perform(
      get("/api/v1/users/{userId}/fields", userId.toString())
-        .param("pageNumber", pageNumber.toString())
-        .param("pageSize", pageSize.toString())
-        .param("sortAttribute", sortAttribute)
-        .param("sortOrder", sortOrder)
+        .param("pageNumber", "0")
+        .param("pageSize", "2")
+        .param("sortAttribute", "createdAt")
+        .param("sortOrder", "desc")
     )
      .andExpect(status().isBadRequest)
      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -35,40 +30,32 @@ class UserControllerTest : BaseIntegrationTest()  {
 @Sql("/scripts/fields/insert_fields.sql")
  fun `should return ok when user has fields`() {
    val userId = UUID.fromString("58fae25b-ea38-4e7b-ab2d-9f555a67836b")
-   val pageNumber = 0
-   val pageSize = 2
-   val sortAttribute = "createdAt"
-   val sortOrder = "desc"
 
    mockMvc.perform(
     get("/api/v1/users/{userId}/fields", userId.toString())
-     .param("pageNumber", pageNumber.toString())
-     .param("pageSize", pageSize.toString())
-     .param("sortAttribute", sortAttribute)
-     .param("sortOrder", sortOrder)
+        .param("pageNumber", "0")
+        .param("pageSize", "2")
+        .param("sortAttribute", "createdAt")
+        .param("sortOrder", "desc")
    ).andExpect(status().isOk)
-     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-    .andExpect(jsonPath("$.content.length()").value(1))
+       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+       .andExpect(jsonPath("$.content.length()").value(1))
  }
 
  @Test
  @Sql("/scripts/fields/insert_fields.sql")
  fun `should return ok when user does not have fields`() {
    val userId = UUID.fromString("68fae25b-ea38-4e7b-ab2d-9f555a67836e")
-   val pageNumber = 0
-   val pageSize = 2
-   val sortAttribute = "createdAt"
-   val sortOrder = "desc"
 
    mockMvc.perform(
     get("/api/v1/users/{userId}/fields", userId.toString())
-     .param("pageNumber", pageNumber.toString())
-     .param("pageSize", pageSize.toString())
-     .param("sortAttribute", sortAttribute)
-     .param("sortOrder", sortOrder)
-   ).andDo(MockMvcResultHandlers.print()) // Califica el método print()
-       .andExpect(status().isOk)
+        .param("pageNumber", "0")
+        .param("pageSize", "2")
+        .param("sortAttribute", "createdAt")
+        .param("sortOrder", "desc")
+   ).andExpect(status().isOk)
      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+       .andExpect(jsonPath("$.content.length()").value(0))
  }
 
 }
