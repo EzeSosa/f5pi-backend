701fb84fa41172ba6d5ffec68577ce5cd91b207d
Martín Jesús Crosetto <martincrosetto312@gmail.com>
2025-05-27 00:12:13 -0300

    ref: unnecesary test were removed

--- src/test/kotlin/com/esosa/f5pi_backend/services/implementations/UserServiceTest.kt
+++ src/test/kotlin/com/esosa/f5pi_backend/services/implementations/UserServiceTest.kt
@@ -9,11 +9,13 @@ import com.esosa.f5pi_backend.data.enums.Role
 import com.esosa.f5pi_backend.data.models.Field
 import com.esosa.f5pi_backend.data.models.Season
 import com.esosa.f5pi_backend.data.models.User
+import com.esosa.f5pi_backend.data.repositories.IPlayerRepository
 import com.esosa.f5pi_backend.data.repositories.IUserRepository
 import com.esosa.f5pi_backend.services.interfaces.IFieldService
 import com.esosa.f5pi_backend.services.interfaces.IGameService
 import com.esosa.f5pi_backend.services.interfaces.IPlayerService
 import com.esosa.f5pi_backend.services.interfaces.ISeasonService
+import com.esosa.f5pi_backend.utils.PageMapper
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -31,6 +33,15 @@ import java.util.UUID
 @ExtendWith(MockitoExtension::class)
 class UserServiceTest {
 
+    object UserServiceTestData {
+        val TEST_USER = User(
+            username = "testUser",
+            password = "testPassword",
+            fullName = "testFullName",
+            email = "test@email.com",
+            role = Role.USER,
+        )
+    }
 
     @Mock
     lateinit var userRepository: IUserRepository
@@ -47,287 +58,41 @@ class UserServiceTest {
     @Mock
     lateinit var playerService: IPlayerService
 
+    @Mock
+    lateinit var playerRepository: IPlayerRepository
+
     @InjectMocks
     lateinit var userService: UserService
 
     @Test
     fun saveUser() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER,
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
 
-        val result = userService.saveUser(user)
+        `when`(userRepository.save(UserServiceTestData.TEST_USER)).thenReturn(UserServiceTestData.TEST_USER)
 
-        assertEquals(user.username, result.username)
-        assertEquals(user.password, result.password)
-    }
+        val result = userService.saveUser(UserServiceTestData.TEST_USER)
 
-    @Test
-    fun getUserPlayers() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER,
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
-        val testPlayers = listOf(
-            PlayerResponse(playerId = UUID.randomUUID(), name = "testPlayer", imageURL = "www.url.com"),
-            PlayerResponse(playerId = UUID.randomUUID(), name = "testPlayer2", imageURL = "www.url2.com"),
-        )
-        val playersPage = PageImpl(testPlayers)
-        `when`(userRepository.findById(result1.id)).thenReturn(Optional.of(result1))
-        `when`(
-            playerService.getUserPlayers(
-                result1,
-                0,
-                10,
-                "name",
-                "asc"
-            )
-        )
-            .thenReturn(playersPage)
-
-
-        val result2 = userService.getUserPlayers(
-            user.id,
-            0,
-            10,
-            "name",
-            "asc"
-        )
-        assertEquals(2, result2.numberOfElements)
-    }
-
-    @Test
-    fun getUserGames() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER,
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
-
-        val field = Field(
-            name = "testField",
-            user = user,
-            id = UUID.randomUUID(),
-        )
-        `when`(fieldService.findFieldByIdOrThrowException(field.id)).thenReturn(field)
-
-        val season = Season(
-            name = "testSeason",
-            initialDate = LocalDate.now().minusDays(30),
-            finalDate = LocalDate.now(),
-            user = user,
-            createdAt = LocalDateTime.now(),
-            id = UUID.randomUUID()
-        )
-        `when`(seasonService.findSeasonByIdOrThrowException(season.id)).thenReturn(season)
-
-
-        val testGames = listOf(
-            GameResponse(UUID.randomUUID(), LocalDate.now(), 1235.00, "Almagro", "2025"),
-            GameResponse(UUID.randomUUID(), LocalDate.now().minusDays(1), 1235.00, "Alumni", "2025"),
-        )
-
-        val gamesPage = PageImpl(testGames)
-
-        `when`(userRepository.findById(result1.id)).thenReturn(Optional.of(result1))
-        `when`(
-            gameService.getGamesByUser(
-                result1,
-                LocalDate.now().minusDays(1),
-                LocalDate.now(),
-                field,
-                season,
-                0,
-                10,
-                "name",
-                "asc"
-            )
-        ).thenReturn(gamesPage)
-
-        val result2 = userService.getUserGames(
-            user.id,
-            LocalDate.now().minusDays(1),
-            LocalDate.now(),
-            field.id,
-            season.id,
-            0,
-            10,
-            "name",
-            "asc"
-        );
-
-        assertEquals(2, result2.numberOfElements)
-    }
-
-    @Test
-    fun getUserFields() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER,
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
-        `when`(userRepository.findById(result1.id)).thenReturn(Optional.of(result1))
-
-        val testFields = listOf(
-            FieldResponse(
-                fieldId = UUID.randomUUID(),
-                fieldName = "testField",
-            ),
-            FieldResponse(
-                fieldId = UUID.randomUUID(),
-                fieldName = "testField2",
-            )
-        )
-
-        val fieldsPage = PageImpl(testFields)
-
-        `when`(
-            fieldService.getUserFields(
-                result1,
-                0,
-                10,
-                "name",
-                "asc"
-            )
-        ).thenReturn(fieldsPage)
-
-        val result2 = userService.getUserFields(
-            user.id,
-            0,
-            10,
-            "name",
-            "asc"
-        )
-
-        assertEquals(2, result2.numberOfElements)
-    }
-
-    @Test
-    fun getUserSeasons() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER,
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
-        `when`(userRepository.findById(result1.id)).thenReturn(Optional.of(result1))
-
-        val testSeasons = listOf(
-            SeasonResponse(
-                id = UUID.randomUUID(),
-                name = "2025",
-                initialDate = LocalDate.now().minusDays(30),
-                finalDate = LocalDate.now()
-            ),
-            SeasonResponse(
-                id = UUID.randomUUID(),
-                name = "2024",
-                initialDate = LocalDate.now().minusDays(30),
-                finalDate = LocalDate.now()
-            )
-        )
-
-        val seasonsPage = PageImpl(testSeasons)
-
-        `when`(
-            seasonService.getUserSeasons(
-                result1,
-                0,
-                10,
-                "name",
-                "asc"
-            )
-        ).thenReturn(seasonsPage)
-
-        val result2 = userService.getUserSeasons(
-            user.id,
-            0,
-            10,
-            "name",
-            "asc"
-        )
-
-        assertEquals(2, result2.numberOfElements)
+        assertEquals(UserServiceTestData.TEST_USER.username, result.username)
+        assertEquals(UserServiceTestData.TEST_USER.password, result.password)
     }
 
     @Test
     fun updateUser() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
         val updateUserRequest = UpdateUserRequest(
             fullName = "testFullName2",
             email = "test2@email.com",
         )
-        val modifiedUser = User("testUser", "testPassword", "testFullName2", "test2@email.com", user.role)
+        val modifiedUser =
+            User("testUser", "testPassword", "testFullName2", "test2@email.com", UserServiceTestData.TEST_USER.role)
 
-        `when`(userRepository.findById(result1.id)).thenReturn(Optional.of(result1))
-        `when`(userRepository.save(result1)).thenReturn(modifiedUser)
+        `when`(userRepository.findById(UserServiceTestData.TEST_USER.id)).thenReturn(Optional.of(UserServiceTestData.TEST_USER))
+        `when`(userRepository.save(UserServiceTestData.TEST_USER)).thenReturn(modifiedUser)
 
-        val result2 = userService.updateUser(
-            user.id,
-            updateUserRequest
-        )
-        assertEquals(modifiedUser.fullName, result2.fullName)
-        assertEquals(modifiedUser.email, result2.email)
+        userService.updateUser(UserServiceTestData.TEST_USER.id, updateUserRequest)
+            .also {
+                assertEquals(modifiedUser.fullName, it.fullName)
+                assertEquals(modifiedUser.email, it.email)
+            }
 
-    }
-
-    @Test
-    fun deleteUser() {
-        val user = User(
-            username = "testUser",
-            password = "testPassword",
-            fullName = "testFullName",
-            email = "test@email.com",
-            role = Role.USER
-        )
-        `when`(userRepository.save(user)).thenReturn(user)
-
-        val result1 = userService.saveUser(user)
-
-        `when`(userRepository.existsById(result1.id)).thenReturn(true)
-
-        assertDoesNotThrow {
-            userService.deleteUser(result1.id)
-        }
 
     }
 }
