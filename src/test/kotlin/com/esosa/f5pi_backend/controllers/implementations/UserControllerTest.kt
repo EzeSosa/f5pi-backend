d8482386c5bc5429d42d0d5aa79ef5efe35776cb
Martín Jesús Crosetto <martincrosetto312@gmail.com>
2025-05-27 23:29:40 -0300

    feat: integration test cases for players and users for UserController class

--- src/test/kotlin/com/esosa/f5pi_backend/controllers/implementations/UserControllerTest.kt
+++ src/test/kotlin/com/esosa/f5pi_backend/controllers/implementations/UserControllerTest.kt
@@ -8,63 +8,78 @@ import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.util.*
 
-class UserControllerTest : BaseIntegrationTest()  {
+class UserControllerTest : BaseIntegrationTest() {
     companion object UserTestData {
-        val USER_ID_WITH = UUID.fromString("58fae25b-ea38-4e7b-ab2d-9f555a67836b")
-        val USER_ID_WITHOUT = UUID.fromString("68fae25b-ea38-4e7b-ab2d-9f555a67836e")
+        val USER_UUID_WITH = UUID.fromString("58fae25b-ea38-4e7b-ab2d-9f555a67836b")
+        val USER_UUID_WITHOUT = UUID.fromString("68fae25b-ea38-4e7b-ab2d-9f555a67836e")
     }
- @Test
- fun `should return bad request when user does not exists`() {
-  val userId = UUID.randomUUID()
-    mockMvc.perform(
-     get("/api/v1/users/{userId}/fields", userId.toString())
-        .param("pageNumber", "0")
-        .param("pageSize", "2")
-        .param("sortAttribute", "createdAt")
-        .param("sortOrder", "desc")
-    )
-     .andExpect(status().isBadRequest)
-     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-     .andExpect(jsonPath("$.message").value("User with id $userId does not exist"))
- }
 
-
-@Test
-@Sql("/scripts/fields/insert_fields.sql")
- fun `should return ok when user has fields`() {
-   val userId = UUID.fromString("58fae25b-ea38-4e7b-ab2d-9f555a67836b")
-
-   mockMvc.perform(
-    get("/api/v1/users/{userId}/fields",USER_ID_WITH.toString())
-        .param("pageNumber", "0")
-        .param("pageSize", "2")
-        .param("sortAttribute", "createdAt")
-        .param("sortOrder", "desc")
-   ).andExpect(status().isOk)
-       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-       .andExpect(jsonPath("$.content.length()").value(1))
- }
-
- @Test
- @Sql("/scripts/fields/insert_fields.sql")
- fun `should return ok when user does not have fields`() {
-   val userId = UUID.fromString("68fae25b-ea38-4e7b-ab2d-9f555a67836e")
-
-   mockMvc.perform(
-    get("/api/v1/users/{userId}/fields", USER_ID_WITHOUT.toString())
-        .param("pageNumber", "0")
-        .param("pageSize", "2")
-        .param("sortAttribute", "createdAt")
-        .param("sortOrder", "desc")
-   ).andExpect(status().isOk)
-     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-       .andExpect(jsonPath("$.content.length()").value(0))
- }
     @Test
-    fun`should return ok when user has players`() {
-        // Implement test logic here
+    fun `should return bad request when user does not exists and we try to get fields`() {
+        val userId = UUID.randomUUID()
         mockMvc.perform(
-            get("/api/v1/users/{userId}/players", USER_ID_WITH.toString())
+            get("/api/v1/users/{userId}/fields", userId.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("User with id $userId does not exist"))
+    }
+
+
+    @Test
+    @Sql("/scripts/fields/insert_fields.sql")
+    fun `should return ok when user has fields`() {
+
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/fields", USER_UUID_WITH.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        ).andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content.length()").value(1))
+    }
+
+    @Test
+    @Sql("/scripts/fields/insert_fields.sql")
+    fun `should return ok when user does not have fields`() {
+
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/fields", USER_UUID_WITHOUT.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        ).andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content.length()").value(0))
+    }
+
+    @Test
+    fun `should return bad request when user does not exists and we try to get players`() {
+        val userId = UUID.randomUUID()
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/players", userId.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("User with id $userId does not exist"))
+    }
+
+    @Test
+    @Sql("/scripts/players/insert_players.sql")
+    fun `should return ok when user has players`() {
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/players", USER_UUID_WITH.toString())
                 .param("pageNumber", "0")
                 .param("pageSize", "2")
                 .param("sortAttribute", "createdAt")
@@ -74,18 +89,59 @@ class UserControllerTest : BaseIntegrationTest()  {
             .andExpect(jsonPath("$.content.length()").value(1))
 
     }
+
     @Test
+    @Sql("/scripts/players/insert_players.sql")
     fun `should return ok when user does not have players`() {
-        // Implement test logic here
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/players", USER_UUID_WITHOUT.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        ).andExpect { content().contentType(MediaType.APPLICATION_JSON) }
+            .andExpect { jsonPath("$.content.length()").value(0) }
     }
 
     @Test
-    fun`should return ok when user has games` () {
-        // Implement test logic here
+    fun `should return bad request when user does not exists and we try to get games`() {
+        val userId = UUID.randomUUID()
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/fields", userId.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("User with id $userId does not exist"))
     }
 
     @Test
+    @Sql("/scripts/games/insert_games.sql")
+    fun `should return ok when user has games`() {
+
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/games", USER_UUID_WITH.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        ).andExpect { content().contentType(MediaType.APPLICATION_JSON) }
+            .andExpect { jsonPath("$.content.length()").value(1) }
+    }
+
+    @Test
+    @Sql("/scripts/games/insert_games.sql")
     fun `should return ok when user does not have games`() {
-        // Implement test logic here
+        mockMvc.perform(
+            get("/api/v1/users/{userId}/games", USER_UUID_WITH.toString())
+                .param("pageNumber", "0")
+                .param("pageSize", "2")
+                .param("sortAttribute", "createdAt")
+                .param("sortOrder", "desc")
+        ).andExpect { content().contentType(MediaType.APPLICATION_JSON) }
+            .andExpect { jsonPath("$.content.length()").value(0) }
     }
 }
